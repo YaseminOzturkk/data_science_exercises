5f804fec075a473d20b294f9db1c85f2765cb5f4
Yasemin Öztürk <48058898+YaseminOzturkk@users.noreply.github.com>
2023-10-02 13:17:57 +0300

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -5,7 +5,7 @@
 import seaborn as sns
 df = sns.load_dataset("car_crashes")
 df.columns
-df.info
+df.head()
 
 ["NUM_" + col.upper() if df[col].dtype != "O" else col.upper() for col in df.columns]
 
