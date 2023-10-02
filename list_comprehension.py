5eb48188bffeb94cc813a3d2d84172bb27063e57
Yasemin Öztürk <48058898+YaseminOzturkk@users.noreply.github.com>
2023-10-02 13:19:16 +0300

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,6 +1,6 @@
 # LIST COMPREHENSİON EXERCISES
 
-# Görev 1: List Comprehension yapısı kullanarak car_crashes verisindeki numeric değişkenlerin isimlerini büyük harfe çeviriniz be başına NUM ekleyiniz.
+# Görev 1: List Comprehension yapısı kullanarak car_crashes verisindeki numeric değişkenlerin isimlerini büyük harfe çeviriniz ve başına NUM ekleyiniz.
 
 import seaborn as sns
 df = sns.load_dataset("car_crashes")
